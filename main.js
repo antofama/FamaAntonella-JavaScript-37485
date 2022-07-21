1343386726de1d7d3559a7f22f9e4f77145cccae
AntoFamagit <antoefama@gmail.com>
2022-07-21 18:44:07 -0300

    primer entrega

--- main.js
+++ main.js
@@ -1,25 +1,46 @@
-let producto = prompt("Ingrese el nombre del producto que desee comprar 1. Zapatillas 2. Remera 3. Pantalon").toUpperCase();
-let cantidad = parseInt(prompt("Ingrese la cantidad que desea comprar"));
-let totalAPagar = parseInt(prompt("Ingrse el total de su compra"));
-let pago = parseInt(prompt("Indique en las cuotas que desea pagar 1 Cuota 3 Cuotas 6 Cuotas 12 Cuotas"));
+
 let stock = 10;
-let precio = 8000;
+let precio = 2000;
+const carrito = [];
 
+const productos =[
+    {
+        id: 1,
+        producto:"Collar Roma",
+        precio: 2400,
+        stock: 15,
+    },
+    {
+        id: 2,
+        producto: "Pulsera Lisboa",
+        precio: 1900,
+        stock: 10,
+    },
+    {
+        id: 3,
+        producto: "Aros Paris",
+        precio: 1200,
+        stock: 8,
+    }
+];
 
-
+let eleccionProducto = prompt("Ingrese el producto que desea comprar 1.Collar Roma 2.Pulsera Lisboa 3.Aros Paris").toUpperCase();
 function agregarAlCarrito() {
-    if (producto === "ZAPATILLAS" || producto === "REMERA" || producto === "PANTALON") {
-        console.log("Agregaste el procuto al carrito: " + producto);
-        
+    if (eleccionProducto === "COLLAR ROMA" || eleccionProducto === "PULSERA LISBOA" || eleccionProducto === "AROS PARIS") {
+        carrito.push(eleccionProducto)
+        console.log(carrito);        
     }else{
         console.log("No elegiste ninguna opcion");
     } 
+    
 }
-agregarAlCarrito();
+agregarAlCarrito(); 
 
+// Me gustaria saber como valido el stock con los datos del array ???
+let cantidad = parseInt(prompt("Ingrese la cantidad desea comprar del producto"));
 function validarStock() {
-    if(cantidad <= 10){
-        console.log("Vas a comprar la cantidad de: " + cantidad + "de este:" + producto);
+    if(cantidad <= stock){
+        console.log("Contamos con esa cantidad");
     }else{
         console.log("No contamos con esa cantidad");
     }
@@ -27,26 +48,49 @@ function validarStock() {
 }
 validarStock();
 
-function calcularCompra() {
-    const resultado = cantidad * precio
-    alert("el valor de su compra es de:" + resultado)
-    
+function totalCarrito(carrito) {
+    for (let i=0; i < carrito.length; i++){
+        console.log(carrito[i]);
+    }
 }
-calcularCompra();
+totalCarrito(carrito);
 
-function formaDepago(totalAPagar, pago) {
+function eliminarDelCarrito(nombreDelProducto) {
+    const index = carrito.findIndex((productos) => productos.producto === nombreDelProducto)
+    if(index != -1){
+        carrito.splice(index,1)
+        console.log(carrito);
+    }else{
+        alert("Ese producto no esta el carrito");
+
+    }
+}
+let productoEliminado = prompt("Si desea elimimar algo del carrito ingrese el nombre del producto").toUpperCase;
+
+eliminarDelCarrito(productoEliminado);
+
+
+//Aca tengo la misma consulta como deberia hacer para tomar el dato del precio ??
+function calcularCompra(cantidad,precio) {
+    return cantidad * precio
+}
+const resultado = calcularCompra(cantidad,precio);
+alert("Su total a pagar es de:" + resultado)
+
+let pago = parseInt(prompt("Indique en las cuotas que desea pagar 1 Cuota 3 Cuotas 6 Cuotas 12 Cuotas"));
+function formaDepago(resultado, pago) {
     switch (pago) {
         case 1:
-            return alert("En: " + pago + "debe abonar 1 couta de:" + totalAPagar);
+            return alert("En: " + pago + "debe abonar 1 couta de:" + resultado);
             break;
         case 3:
-            return alert("En: " + pago + "debe abonar 1 couta de:" + totalAPagar/3);
+            return alert("En: " + pago + "debe abonar 3 coutas de:" + resultado/3);
             break;
         case 6:
-            return alert("En: " + pago + "debe abonar 1 couta de:" + totalAPagar/6);
+            return alert("En: " + pago + "debe abonar 6 coutas de:" + resultado/6);
             break;
         case 12:
-            return alert("En: " + pago + "debe abonar 1 couta de:" + totalAPagar/12);
+            return alert("En: " + pago + "debe abonar 12 coutas de:" + resultado/12);
             break;
         default:
             alert("No ingreso la cantidad de coutas validas");
@@ -54,5 +98,5 @@ function formaDepago(totalAPagar, pago) {
     }
     
 }
-formaDepago();
+formaDepago(resultado, pago);
 
