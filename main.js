3b11805337af5ba0f01b1ab9ce63f91f79758986
AntoFamagit <antoefama@gmail.com>
2022-07-27 18:48:53 -0300

    correcciones

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
 
-let stock = 10;
-let precio = 2000;
 const carrito = [];
 
 const productos =[
@@ -24,29 +22,26 @@ const productos =[
     }
 ];
 
-let eleccionProducto = prompt("Ingrese el producto que desea comprar 1.Collar Roma 2.Pulsera Lisboa 3.Aros Paris").toUpperCase();
-function agregarAlCarrito() {
-    if (eleccionProducto === "COLLAR ROMA" || eleccionProducto === "PULSERA LISBOA" || eleccionProducto === "AROS PARIS") {
-        carrito.push(eleccionProducto)
-        console.log(carrito);        
-    }else{
-        console.log("No elegiste ninguna opcion");
-    } 
-    
-}
-agregarAlCarrito(); 
-
-// Me gustaria saber como valido el stock con los datos del array ???
+let eleccionProducto = parseInt(prompt("Seleccione el numero que desea comprar 1.Collar Roma 2.Pulsera Lisboa 3.Aros Paris"));
 let cantidad = parseInt(prompt("Ingrese la cantidad desea comprar del producto"));
-function validarStock() {
-    if(cantidad <= stock){
-        console.log("Contamos con esa cantidad");
-    }else{
-        console.log("No contamos con esa cantidad");
-    }
-    
+function agregarAlCarrito() {
+    const found = productos.find(el => el.id === eleccionProducto);
+    const tenemosStock = validarStock(productos, cantidad);
+    carrito.push(eleccionProducto);
 }
-validarStock();
+agregarAlCarrito(console.log(carrito)); 
+
+// Creo que me confunden el tema de los parametros, siempre me tira false 
+
+function validarStock(productos, cantidad){
+    if (cantidad < productos.stock) {
+        return true;
+    }else{
+        return false;
+    } 
+}
+validarStock(productos,cantidad);
+
 
 function totalCarrito(carrito) {
     for (let i=0; i < carrito.length; i++){
@@ -70,27 +65,29 @@ let productoEliminado = prompt("Si desea elimimar algo del carrito ingrese el no
 eliminarDelCarrito(productoEliminado);
 
 
-//Aca tengo la misma consulta como deberia hacer para tomar el dato del precio ??
-function calcularCompra(cantidad,precio) {
-    return cantidad * precio
-}
-const resultado = calcularCompra(cantidad,precio);
-alert("Su total a pagar es de:" + resultado)
+//me sale un error undefined, es porque tengo que declarar una variable Let acumulador = 0;?
 
-let pago = parseInt(prompt("Indique en las cuotas que desea pagar 1 Cuota 3 Cuotas 6 Cuotas 12 Cuotas"));
-function formaDepago(resultado, pago) {
+function calcularCompra() {
+    const total = carrito.reduce((acc,el)=> el.precio, 0)
+    console.log(total);
+}
+calcularCompra();
+
+//La forma de pago no se si es necesario dejarlo o se me complica mas el tema de la logica ?
+
+/* function formaDepago(resultado, pago) {
     switch (pago) {
         case 1:
             return alert("En: " + pago + "debe abonar 1 couta de:" + resultado);
             break;
         case 3:
-            return alert("En: " + pago + "debe abonar 3 coutas de:" + resultado/3);
+            return alert("En: " + pago + "debe abonar 3 couta de:" + resultado/3);
             break;
         case 6:
-            return alert("En: " + pago + "debe abonar 6 coutas de:" + resultado/6);
+            return alert("En: " + pago + "debe abonar 6 couta de:" + resultado/6);
             break;
         case 12:
-            return alert("En: " + pago + "debe abonar 12 coutas de:" + resultado/12);
+            return alert("En: " + pago + "debe abonar 12 couta de:" + resultado/12);
             break;
         default:
             alert("No ingreso la cantidad de coutas validas");
@@ -98,5 +95,4 @@ function formaDepago(resultado, pago) {
     }
     
 }
-formaDepago(resultado, pago);
-
+formaDepago(resultado, pago); */
