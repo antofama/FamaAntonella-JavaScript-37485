b3e4fa5852a8fc0e277ddefa81572f709ac8bb65
AntoFamagit <antoefama@gmail.com>
2022-08-01 23:44:49 -0300

    Dom y Eventos

--- main.js
+++ main.js
@@ -1,56 +1,104 @@
-
 const carrito = [];
-
 const productos =[
     {
         id: 1,
-        producto:"Collar Roma",
+        titulo:"Cafe Colombia",
+        img: "../img/Colombia.jpg", 
         precio: 2400,
         stock: 15,
     },
     {
         id: 2,
-        producto: "Pulsera Lisboa",
+        producto: "Cafe El Salvador",
+        img: "../img/El Salvador.jpg",
         precio: 1900,
         stock: 10,
     },
     {
         id: 3,
-        producto: "Aros Paris",
-        precio: 1200,
+        producto: "Cafe Etiopia",
+        img: "../img/Etiopia.jpg",
+        precio: 2200,
         stock: 8,
+    },
+    {
+        id: 4,
+        producto: "Cafe Guatemala",
+        img: "../img/Guatemala.jpg",
+        precio: 1900,
+        stock: 11,
+    },
+    {
+        id: 5,
+        producto: "Cafe Honduras",
+        img: "../img/Honduras.jpg",
+        precio: 1750,
+        stock: 14,
+    },
+    {
+        id: 6,
+        producto: "Cafe Kenia",
+        img: "../img/Kenia.jpg",
+        precio: 2000,
+        stock: 9,
+    },
+    {
+        id: 7,
+        producto: "Cafe Nicaragua",
+        img: "../img/Nicaragua.jpg",
+        precio: 1600,
+        stock: 7,
+    },
+    {
+        id: 8,
+        producto: "Cafe Peru",
+        img: "../img/Peru.jpg",
+        precio: 1500,
+        stock: 6,
     }
 ];
 
-let eleccionProducto = parseInt(prompt("Seleccione el numero que desea comprar 1.Collar Roma 2.Pulsera Lisboa 3.Aros Paris"));
-let cantidad = parseInt(prompt("Ingrese la cantidad desea comprar del producto"));
-function agregarAlCarrito() {
-    const found = productos.find(el => el.id === eleccionProducto);
-    const tenemosStock = validarStock(productos, cantidad);
-    carrito.push(eleccionProducto);
+//Generamos las Cards.
+
+productos.forEach((producto) =>{
+    const idButton = `add-carts${producto.id}`
+    document.getElementById("seccion-cards").innerHTML += `<div class="col mb-5">
+    <div class="card h-100">
+        <img class="card-img-top" src="${producto.img}" />
+        <div class="card-body p-4">
+            <div class="text-center">
+                <h5 class="fw-bolder">${producto.producto}</h5>
+                <div class="precio">
+                    <p>$${producto.precio}</p>
+                </div>
+            </div>
+        </div>
+        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+            <div class="text-center"><a class="btn btn-outline-dark mt-auto" id="${idButton}">Agregar al carrito</a></div>
+        </div>
+    </div>
+</div>`
+})
+
+productos.forEach((producto) => {
+    const idButton =`add-carts${producto.id}`
+    document.getElementById(idButton) .onclick = ()=>{
+    carrito.push(producto);
+    document.getElementById("cart-total").innerHTML = carrito.length;   
+    console.log(carrito);
 }
-agregarAlCarrito(console.log(carrito)); 
+});
 
-// Creo que me confunden el tema de los parametros, siempre me tira false 
-
-function validarStock(productos, cantidad){
-    if (cantidad < productos.stock) {
-        return true;
-    }else{
-        return false;
-    } 
-}
-validarStock(productos,cantidad);
-
-
+// entiendo qu esta funcion ya no se necesita porque la utilice arriba
+/* 
 function totalCarrito(carrito) {
     for (let i=0; i < carrito.length; i++){
         console.log(carrito[i]);
     }
 }
-totalCarrito(carrito);
+totalCarrito(carrito); */
 
-function eliminarDelCarrito(nombreDelProducto) {
+/* function eliminarDelCarrito() {
     const index = carrito.findIndex((productos) => productos.producto === nombreDelProducto)
     if(index != -1){
         carrito.splice(index,1)
@@ -60,18 +108,16 @@ function eliminarDelCarrito(nombreDelProducto) {
 
     }
 }
-let productoEliminado = prompt("Si desea elimimar algo del carrito ingrese el nombre del producto").toUpperCase;
-
-eliminarDelCarrito(productoEliminado);
-
+eliminarDelCarrito();
+ */
 
 //me sale un error undefined, es porque tengo que declarar una variable Let acumulador = 0;?
-
+ 
 function calcularCompra() {
-    const total = carrito.reduce((acc,el)=> el.precio, 0)
+    const total = carrito.reduce((acumulador,producto) => acumulador + producto.precio,0)
     console.log(total);
 }
-calcularCompra();
+calcularCompra(); 
 
 //La forma de pago no se si es necesario dejarlo o se me complica mas el tema de la logica ?
 
